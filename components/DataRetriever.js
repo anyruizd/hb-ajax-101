0c96684e792b5b718b08a4ed4c8f4e321cbdabb5
anyruizd <anyruizd@gmail.com>
2018-01-19 11:45:35 -0500

    data retriever doesn't work

---
+++ components/DataRetriever.js
@@ -0,0 +1,10 @@
+export default class DataRetriever {
+    console.log('DataRetriever!')
+    /* static get (url, callback) {
+    var oReq = new XMLHttpRequest();
+    oReq.addEventListener('load', callback);
+    oReq.responseType = 'json'
+    oReq.open('GET', url);
+    oReq.send();
+    } */
+}
